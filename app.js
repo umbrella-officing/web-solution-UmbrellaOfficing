3d5b8ba32074f1f981882c6135573bb36bb9bfac
Felipe <felipegstos09@gmail.com>
2022-07-21 23:25:04 -0300

    Fazendo tela de titulo e descricao do local

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000;
 const home = require('./app/routes/index')(app);
 const addRoom = require('./app/routes/add-room')(app);
 const officingFeatures = require('./app/routes/officing-features')(app);
+const apresentation = require('./app/routes/apresentation')(app);
 
 app.listen(port,()=>{
     console.log(`Servidor no ar rodando na porta ${port}`)
