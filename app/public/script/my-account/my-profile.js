9f22b21fb7d4016e228111559349929dd66f8f65
Luna <rodriguesjulia107@gmail.com>
2022-07-26 22:29:16 -0300

    Telas: Minha Conta atualizando

---
+++ app/public/script/my-account/my-profile.js
@@ -0,0 +1,14 @@
+// const chooseOption = () => {
+//     const options_menu = document.querySelectorAll(".options_menu");
+//     options_menu.forEach(option => {
+//         option.addEventListener("click", checked) 
+//     });
+//     function checked() {
+//         options_menu.forEach(option => {
+//             option.classList.remove("chooseOption")
+//         });
+//         this.classList.add("chooseOption")
+//     };
+// };
+
+// chooseOption();
