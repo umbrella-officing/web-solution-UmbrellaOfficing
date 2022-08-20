0ad60370ac1fd5bfb941f5bd109255fd40033450
Luna <rodriguesjulia107@gmail.com>
2022-08-20 17:36:10 -0300

    Minhas reservas - popUp

--- app/public/script/my-account/reservations.js
+++ app/public/script/my-account/reservations.js
@@ -11,5 +11,28 @@ const openPopCancel = () => {
     })
     })
 
-    
 }
+
+openPopCancel();
+
+const popCancel_state = () => {
+    const buttonGo= document.querySelectorAll(".go");
+    const popCancel_state1 = document.getElementById("popUp-cancel_state1");
+    const popCancel_state3 = document.getElementById("popUp-cancel_state3");
+
+    
+    console.log(buttonGo[0])
+
+    buttonGo.forEach((element,index)=> {
+        element.addEventListener('click', () => {
+        popCancel_state1.style.display = 'flex'
+
+        console.log(index)
+    })
+    })
+
+}
+
+popCancel_state();
+
+openPopCancel();
