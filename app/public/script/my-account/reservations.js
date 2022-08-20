deb72f7d3eb227d558a5921a35c36996c8c4ae7b
UmbrellaOfficing <felipemendescp125@gmail.com>
2022-08-20 18:11:07 -0300

    refresh

--- app/public/script/my-account/reservations.js
+++ app/public/script/my-account/reservations.js
@@ -2,14 +2,28 @@
 const openPopCancel = () => {
     const buttonCancel= document.querySelectorAll(".cancel");
     const container_popUp_cancel= document.getElementById("container_popUp_cancel");
+    const popUp_flex = document.querySelectorAll('.popUp_cancel form > div')
+    console.log(popUp_flex)
 
     console.log({buttonCancel})
 
-    buttonCancel.forEach((element,index)=> {
-        element.addEventListener('click', () => {
-        container_popUp_cancel.style.display = 'flex'
-        console.log(index)
+    buttonGo.forEach((buttonGo,index)=>{
+        buttonGo.addEventListener('click',()=>{
+            states(buttonGo,index)
+        })
     })
-    })
+
+    function states(buttonGo,index){
+
+            if(index == index){
+                var buttonGo = buttonGo.childNodes[1]
+                state2.style.display = 'flex'
+
+        
+
+                console.log(button1.value++)
+
+            }
+        }
 }
 openPopCancel()
