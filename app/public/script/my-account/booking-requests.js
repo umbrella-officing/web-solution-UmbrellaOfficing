3c9548f7e0bf1a4bb5859006348dd3b4e94f39e1
Luna <rodriguesjulia107@gmail.com>
2022-08-29 23:34:00 -0300

    Atualizando pop-ups

--- app/public/script/my-account/booking-requests.js
+++ app/public/script/my-account/booking-requests.js
@@ -70,3 +70,33 @@ function closePopAvaliation(avaliationOwner) {
 }
 
 openPopAvaliation()
+
+const rating = () => {
+
+    const listStars = document.querySelector('.rating')
+    const stars = document.querySelectorAll(".star")
+
+    stars.forEach((item, index, arr)=>{
+        item.addEventListener('click',(e)=>{
+         
+            const current_star_level = index + 1
+            const lengthStars = arr.length
+            
+            if(index == index){
+                for (let i = 0; i <= index; i++) {
+                  arr[i].setAttribute('src','images/search-spaces/search/background-star.png')
+                }
+            }
+
+            if(current_star_level < lengthStars){
+            for (let i = index+1; i < lengthStars; i++) {
+                arr[i].setAttribute('src','images/search-spaces/search/star.png')
+              }
+            }
+
+        })
+    })
+
+}
+
+rating()
