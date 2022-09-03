f41b3add4d5e1ff62f55f09d8f0b8266a8c77124
Felipe <felipegstos09@gmail.com>
2022-09-03 17:05:32 -0300

    Detalhes da home e jquery da central de ajuda

--- app/public/script/help-center/help-center.js
+++ app/public/script/help-center/help-center.js
@@ -1,23 +1,15 @@
-// $(document).ready(function(){
-//     $("#forCustomers").click(function(){
-//         $("#forCustomers").css("color", "var(--scale-5-blue)");
-//         $("#forAdvertisers").css("color", "var(--text-1-black)");
-//     });
+$(document).ready(function(){
+    $("#forCustomers").click(function(){
+        $("#forCustomers").css("color", "var(--scale-5-blue)");
+        $("#forAdvertisers").css("color", "var(--text-1-black)");
+        $("align-options:after").animate({left: '0'});
+    });
 
-//     $("#forAdvertisers").click(function(){
-//         $("#forAdvertisers").css("color", "var(--scale-5-blue)");
-//         $("#forCustomers").css("color", "var(--text-1-black)");
-//     })
-// })
+    $("#forAdvertisers").click(function(){
+        $("#forAdvertisers").css("color", "var(--scale-5-blue)");
+        $("#forCustomers").css("color", "var(--text-1-black)");
+        $("align-options:after").animate({right: '0'});
+    })
 
-const alternate = document.querySelectorAll(".btn")
+});
 
-alternate.forEach(btn =>{
-    btn.addEventListener("click", checked)
-})
-
-function checked(){
-    this.classList.add("")
-}
-
-console.log(alternate);
