0bef647555ff7daa18738e6e5083364361b01334
Felipe Xavier Gomes dos Santos <rm79776@estudante.fieb.edu.br>
2022-09-05 11:45:18 -0300

    último commit de SEWE

--- app/public/script/help-center/help-center.js
+++ app/public/script/help-center/help-center.js
@@ -2,13 +2,19 @@ $(document).ready(function(){
     $("#forCustomers").click(function(){
         $("#forCustomers").css("color", "var(--scale-5-blue)");
         $("#forAdvertisers").css("color", "var(--text-1-black)");
-        $("align-options:after").animate({left: '0'});
+        $("div::after").animate({left: '0'});
     });
+
+    // $("#forCustomers").ready(function(){
+    //     $("#forCustomers").css("color", "var(--scale-5-blue)");
+    //     $("#forAdvertisers").css("color", "var(--text-1-black)");
+    //     $("align-options:after").animate({left: '0'});
+    // });
 
     $("#forAdvertisers").click(function(){
         $("#forAdvertisers").css("color", "var(--scale-5-blue)");
         $("#forCustomers").css("color", "var(--text-1-black)");
-        $("align-options:after").animate({right: '0'});
+        $("div::after").animate({right: '0'});
     })
 
 });
