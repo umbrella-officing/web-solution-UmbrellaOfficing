f5741d1814be30f0860bc62ee4bee50c6f3137de
Felipe <felipegstos09@gmail.com>
2022-09-27 23:54:41 -0300

    Tela type-of-environment: responsivo visualmente pronto

--- app/public/script/register-space/type-of-environment.js
+++ app/public/script/register-space/type-of-environment.js
@@ -1,17 +1,17 @@
- function hint(x){
-     if (x === 1){
-         document.getElementById("hint-adaptable").style.display="flex";
-     }
-     else if (x === 2){
-         document.getElementById("hint-corporate").style.display="flex";
-     }
-     else if (x === 3){
-         document.getElementById("hint-adaptable").style.display="none";
-     }
-     else{
-         document.getElementById("hint-corporate").style.display="none";
-     }
- }
+//  function hint(x){
+//      if (x === 1){
+//          document.getElementById("hint-adaptable").style.display="flex";
+//      }
+//      else if (x === 2){
+//          document.getElementById("hint-corporate").style.display="flex";
+//      }
+//      else if (x === 3){
+//          document.getElementById("hint-adaptable").style.display="none";
+//      }
+//      else{
+//          document.getElementById("hint-corporate").style.display="none";
+//      }
+//  }
 
 
 // function openHint(){
@@ -22,3 +22,15 @@
 // function closeHint(){
 //     const hintIcon = document.()
 // }
+var corporate = document.getElementById("corporate");
+
+function foco(){
+    corporate.addEventListener("click", () =>{
+    corporate.style = 'border-color: #08bdff;'
+    'box-shadow: 0 0 0 0.2rem rgb(0, 123, 255, .25);'
+    'transition: 0.3s ease-in-out;'
+    })
+ // window.addEventListener("load", foco);
+}
+
+document.load(foco());
