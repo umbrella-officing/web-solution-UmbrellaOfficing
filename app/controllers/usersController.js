9b25f43275e37c40d765f6b5865f253ec79a9760
UmbrellaOfficing <felipemendescp125@gmail.com>
2022-09-28 22:28:12 -0300

    sistema sign up e login finalizados

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -1,12 +1,20 @@
 var bcrypt = require("bcryptjs");
+const { application } = require("express");
 var salt = bcrypt.genSaltSync(12);
 
+module.exports.central = (application, req, res) => {
+    if (req.session.autenticado) {
+      autenticado = { autenticado: req.session.usu_autenticado };
+    } else {
+      autenticado = { autenticado: null };
+    }
+    res.render("teste/central",autenticado);
+}
 
 module.exports.index = (application, req, res) => {
     const connection = application.config.dbConnection;
     const noticiasDao = new application.app.models.UsersDAO(connection);
     noticiasDao.get5UltimasNoticias((error, result) => {
-      console.log(result)
       res.render("teste/register",
       {
        sala:result,
@@ -16,6 +24,10 @@ module.exports.index = (application, req, res) => {
     });
   }
 
+  module.exports.sair = (application, req, res) =>{
+    req.session.destroy();
+    res.redirect("/central");
+  }
 
 module.exports.registerUser = (application, req, res) =>{
 
@@ -27,9 +39,13 @@ module.exports.registerUser = (application, req, res) =>{
     senha_usuario: bcrypt.hashSync(req.body.senha_usu, salt),
     nome_usuario: req.body.nome_usu,
     email_usuario: req.body.email_usu,
+    data_escolhida:req.body.data_escolhida
     };
 
   req.assert('nome_usu', 'Mínimo 3 caracters').len(3, 100);
+  req.assert('data_escolhida', 'Formato da data está inadequado').isDate({
+    format: 'YYYY-MM-DD'
+});
 
   const erros = req.validationErrors()
 
@@ -73,7 +89,10 @@ module.exports.formLogin = (application, req, res) =>{
         
         if (total == 1) {
           if (bcrypt.compareSync(dadosForm.senha_usuario,results[0].senha_usuario)) {
-              return res.redirect("/login");
+            req.session.autenticado = true;
+            req.session.usu_autenticado = results[0].nome_usuario;
+            console.log(results[0].nome_usuario)
+            return res.redirect("/central");
           }
         }
         res.redirect("/cadastro");
