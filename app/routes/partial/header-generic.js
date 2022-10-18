9e322150fd9003d1544af70f6ceb40bce70d996c
UmbrellaOfficing <felipemendescp125@gmail.com>
2022-10-18 15:16:22 -0300

    bugs corrigidos

--- app/routes/partial/header-generic.js
+++
@@ -1,5 +0,0 @@
-module.exports = (app) =>{
-    app.get('/header-generico', (req, res) =>{
-        res.render('./partial/header-generic')
-    });
-}
--- app/routes/partial/header-register-space.js
+++
@@ -1,5 +0,0 @@
-module.exports = (app) =>{
-    app.get('/header-adicionar-espaco', (req, res) =>{
-        res.render('./partial/header-register-space')
-    });
-}
