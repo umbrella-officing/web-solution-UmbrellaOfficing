066f33203d27b9e46bc1b3e0fbddca02d5390ae1
Luna <rodriguesjulia107@gmail.com>
2022-10-20 17:47:04 -0300

    Torne-se um anfitrião

--- app/public/script/become-advertiser.js
+++ app/public/script/become-advertiser.js
@@ -4,7 +4,7 @@ $('.slider').slick({
     infinite: true,
     slidesToScroll: 1,
     slidesToShow: 1,
-    arrows: true,
+    arrows: false,
     prevArrow: "<button class='arrows'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
     nextArrow: "<button class='arrows arrow-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
     draggable: false,
